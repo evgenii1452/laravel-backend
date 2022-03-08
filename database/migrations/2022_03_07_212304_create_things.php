ab228488b044222f7dc09237c78e2b8714e10ee7
Lavashick <lavashick@icloud.com>
2022-03-09 00:32:03 +0300

    Миграция создания таблицы things

---
+++ database/migrations/2022_03_07_212304_create_things.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('things', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->dateTime('wrnt');
+            $table->unsignedBigInteger('master');
+            $table->foreign('master')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('things');
+    }
+};
